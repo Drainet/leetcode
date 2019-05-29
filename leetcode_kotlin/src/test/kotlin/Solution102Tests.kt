cc535de82fed9dd12faee4b4a036b08f5748c66b
drain <drain.cy.chang@gmail.com>
2019-05-29 19:55:47 +0800

    add kotlin 102

---
+++ leetcode_kotlin/src/test/kotlin/Solution102Tests.kt
@@ -0,0 +1,20 @@
+package main.kotlin
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Solution102Tests {
+
+    @Test
+    fun test1() {
+        val node = TreeNode(3).apply {
+            left = TreeNode(9)
+            right = TreeNode(20).apply {
+                left = TreeNode(15)
+                right = TreeNode(7)
+            }
+        }
+        val result = Solution102().levelOrder(node)
+        assertEquals(listOf(listOf(3), listOf(9, 20), listOf(15, 7)), result)
+    }
+}
