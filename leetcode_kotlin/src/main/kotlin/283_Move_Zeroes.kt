2487a46cfe3e3adfb7348abd2a25f97d00f375e9
drain <drain.cy.chang@gmail.com>
2019-06-04 23:10:14 +0800

    add solution 283 and test

--- leetcode_kotlin/src/main/kotlin/283_Move_Zeroes.kt
+++ leetcode_kotlin/src/main/kotlin/283_Move_Zeroes.kt
@@ -1,7 +1,17 @@
 package main.kotlin
 
 class Solution283 {
-    fun moveZeroes(nums: IntArray): Unit {
-        TODO()
+    fun moveZeroes(nums: IntArray) {
+        var zeroStartIndex = 0
+        for (i in 0 until nums.size) {
+            val num = nums[i]
+            if (num != 0) {
+                if (i != zeroStartIndex) {
+                    nums[zeroStartIndex] = num
+                    nums[i] = 0
+                }
+                zeroStartIndex++
+            }
+        }
     }
 }
--- leetcode_kotlin/src/test/kotlin/Solution283Tests.kt
+++ leetcode_kotlin/src/test/kotlin/Solution283Tests.kt
@@ -1,12 +1,15 @@
-import main.kotlin.Solution102
-import main.kotlin.TreeNode
-import org.junit.jupiter.api.Assertions.assertEquals
+import main.kotlin.Solution283
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class Solution283Tests {
 
     @Test
     fun test1() {
-
+        val results = intArrayOf(0, 1, 0, 3, 12)
+        Solution283().moveZeroes(results)
+        assertTrue(results.contentEquals(intArrayOf(1, 3, 12, 0, 0)))
     }
+
+
 }
