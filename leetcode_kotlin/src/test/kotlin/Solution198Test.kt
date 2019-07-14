55dccacc0d48623e9e939937eda23f81a7946470
drain <drain.cy.chang@gmail.com>
2019-07-14 17:43:26 +0800

    solution 198 and test

---
+++ leetcode_kotlin/src/test/kotlin/Solution198Test.kt
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Solution198Test  {
+    @Test
+    fun test1() {
+        val result = Solution198().rob(intArrayOf(1, 2, 3, 1))
+        assertEquals(4, result)
+    }
+
+    @Test
+    fun test2() {
+        val result = Solution198().rob(intArrayOf(2,7,9,3,1))
+        assertEquals(12, result)
+    }
+}
