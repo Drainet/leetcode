04ca1393803f112ff03cf8ebe5a2383c757fb053
drain <drain.cy.chang@gmail.com>
2019-07-14 18:21:01 +0800

    refactor 198

--- leetcode_kotlin/src/main/kotlin/198_House_Robber.kt
+++ leetcode_kotlin/src/main/kotlin/198_House_Robber.kt
@@ -2,22 +2,17 @@ import kotlin.math.max
 
 class Solution198 {
     fun rob(nums: IntArray): Int {
-        if (nums.size <= 3) {
-            return max(nums.getOrElse(nums.size - 2) { 0 },
-                nums.getOrElse(nums.size - 1) { 0 } +
-                        nums.getOrElse(nums.size - 3) { 0 }
-            )
+        if (nums.size <= 2) {
+            return nums.max() ?: 0
         }
-        var prev1Max = max(nums[nums.size - 2], nums[nums.size - 1] + nums[nums.size - 3])
-        var prev2Max = nums[nums.size - 2]
-        var prev3Max = nums[nums.size - 1]
-        for (i in nums.size - 4 downTo 0) {
+        var prev2 = nums[nums.size - 1]
+        var prev1 = max(nums[nums.size - 2], prev2)
+        for (i in nums.size - 3 downTo 0) {
             val num = nums[i]
-            val currentMax = max(max(num + prev2Max, prev1Max), num + prev3Max)
-            prev3Max = prev2Max
-            prev2Max = prev1Max
-            prev1Max = currentMax
+            val currentMax = max(num + prev2, prev1)
+            prev2 = prev1
+            prev1 = currentMax
         }
-        return max(prev1Max, prev2Max)
+        return max(prev1, prev2)
     }
 }
--- leetcode_kotlin/src/test/kotlin/Solution198Test.kt
+++ leetcode_kotlin/src/test/kotlin/Solution198Test.kt
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class Solution198Test  {
+class Solution198Test {
     @Test
     fun test1() {
         val result = Solution198().rob(intArrayOf(1, 2, 3, 1))
@@ -10,7 +10,13 @@ class Solution198Test  {
 
     @Test
     fun test2() {
-        val result = Solution198().rob(intArrayOf(2,7,9,3,1))
+        val result = Solution198().rob(intArrayOf(2, 7, 9, 3, 1))
         assertEquals(12, result)
+    }
+
+    @Test
+    fun test3() {
+        val result = Solution198().rob(intArrayOf(2, 1, 1, 2))
+        assertEquals(4, result)
     }
 }
